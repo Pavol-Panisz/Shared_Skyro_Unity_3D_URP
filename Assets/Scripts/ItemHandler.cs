e3aac6390f7252931c247a05bb542d3418140712
MartinArthur <martin.arthur.golis@gmail.com>
2025-11-06 13:52:05 +0100

    Update 1.1

--- Assets/Scripts/ItemHandler.cs
+++ Assets/Scripts/ItemHandler.cs
@@ -2,20 +2,54 @@ using NUnit.Framework;
 using UnityEngine;
 using System.Collections.Generic;
 using TMPro;
+using System;
+using UnityEngine.iOS;
+using System.Text.RegularExpressions;
 
 public class ItemHandler : MonoBehaviour
 {
     public List<string> itemNames;
+    public List<string> itemAmounts;
+
+    public GameObject itemContainer;
+    public GameObject itemText;
+
     public TextMeshProUGUI nameInput;
+    public TextMeshProUGUI amountInput;
 
     private void Awake()
     {
-
     }
+
     public void AddItem()
     {
+        TextMeshProUGUI itemObjectInput = itemText.GetComponent<TextMeshProUGUI>();
+
+
         string name = nameInput.text;
-        itemNames.Add(name);
-        Debug.Log("coc");
+        string amount = amountInput.text;
+
+        bool getName = itemNames.Contains(name);
+        if (getName)
+        {
+            int index = itemNames.IndexOf(name, 0);
+        }
+        else if (!getName)
+        {
+            itemAmounts.Add(amount);
+            itemNames.Add(name);
+
+            TextMeshProUGUI itemAmountObject = Instantiate(itemObjectInput, itemContainer.transform);
+            string itemValue = name + " - " + amount;
+            itemAmountObject.text = itemValue;
+        }
+
+
+
+
+
+
+        Debug.Log(name);
+        Debug.Log(amount);
     }
 }
