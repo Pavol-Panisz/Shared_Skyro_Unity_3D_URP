8ef0482b1f36979b23a0b43524f0e3ce576ebc87
DeadlyMouse <elellol007@gmail.com>
2025-11-06 13:52:26 +0100

    AAAAAAAAAA

--- Assets/ArraysScript.cs
+++ Assets/ArraysScript.cs
@@ -1,19 +1,67 @@
 using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UIElements;
 
 public class ArraysScript : MonoBehaviour
 {
-    public List<string> Itemnames;
+    public GameObject Text;
+    public TMP_InputField AddNameInput;
+    public TMP_InputField AddCountInput;
+    public TMP_InputField RemoveNameInput;
+    public TMP_InputField RemoveCountInput;
+
+    public GameObject ItemNamesContent;
+    public GameObject ItemCountContent;
+
+    public List<string> ItemNames;
     public List<int> ItemCount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void AddItem()
+    {
+        if (AddNameInput.text != "" && AddCountInput.text != "")
+        {
+            int CountOfItems = int.Parse(AddCountInput.text);
+            for (int i = 0; i < ItemNames.Count; i++)
+            {
+                if (AddNameInput.Equals(ItemNames[i]))
+                {
+                    ItemCount[i] -= CountOfItems;
+                }
+                else
+                {
+                    GameObject createdNameText = Instantiate(Text, ItemNamesContent.transform);
+                    GameObject createdCountText = Instantiate(Text, ItemCountContent.transform);
+
+                    createdNameText.GetComponent<TMP_Text>().text = AddNameInput.text;
+                    createdCountText.GetComponent<TMP_Text>().text = CountOfItems.ToString();
+
+                    ItemNames.Add(createdNameText.GetComponent<TMP_Text>().text);
+                    ItemCount.Add(CountOfItems);
+                }
+            }
+        }
+    }
+    public void RemoveItem()
+    {
+        for (int i = 0; i < ItemNames.Count; i++)
+        {
+            if (RemoveNameInput.Equals(ItemNames[i]) && RemoveCountInput.Equals(ItemCount[i]))
+            {
+                ItemNames.RemoveAt(i);
+                ItemCount.RemoveAt(i);
+            }
+        }
     }
 }
