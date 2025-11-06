3b1dd28cfaf4688f1f9c48a6de3ad08959471c26
Pavol-Panisz <pavol.panisz@gmail.com>
2025-11-06 20:45:40 +0100

    wip lists storage
    front end done, backend just started

---
+++ Assets/ListsStorage.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+
+public class ListsStorage
+{
+    public List<string> itemNames = new();
+    public List<int> itemCounts = new();
+
+    public void AddItem(string itemName, int itemCount)
+    {
+        
+    }
+
+    public void RemoveItem()
+    {
+        
+    }
+}
