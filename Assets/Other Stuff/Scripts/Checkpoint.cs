24e9e57ce30107e27ebe1394dcec0798d444309d
Pavol-Panisz <pavol.panisz@gmail.com>
2025-11-13 11:50:42 +0100

    researching lighting and terrain

---
+++ Assets/Other Stuff/Scripts/Checkpoint.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class Checkpoint : MonoBehaviour
+{
+    public float spawnOffset = 3f;
+    public UnityEvent OnThisSetAsRespawn;
+    public UnityEvent OnOtherSetAsRespawn;
+
+    Checkpoint[] allCheckpoints;
+
+    private void Start()
+    {
+        allCheckpoints = GameObject.FindObjectsOfType<Checkpoint>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            KillPlayer.playerRespawnPoint = transform.position + Vector3.up * spawnOffset;
+            
+            foreach (var checkpoint in allCheckpoints)
+            {
+                if (checkpoint == this)
+                {
+                    OnThisSetAsRespawn?.Invoke();
+                }
+                else
+                {
+                    checkpoint.OnOtherSetAsRespawn?.Invoke();
+                }
+            }
+
+        }
+    }
+}
