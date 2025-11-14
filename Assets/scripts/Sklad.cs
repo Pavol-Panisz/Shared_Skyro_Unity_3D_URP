279d73ad973d2e724cca71b1704abc718941dd0e
MarPalko <palus.marrtin@gmail.com>
2025-11-14 10:35:21 +0100

    dalsie listy

--- Assets/scripts/Sklad.cs
+++ Assets/scripts/Sklad.cs
@@ -2,21 +2,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Runtime.Serialization;
+using System.Diagnostics;
 
 public class Sklad : MonoBehaviour
 {
+
+    [Header("Item names list")]
     public List<string> userInputList = new List<string>();
     string userInputText;
     bool isItemIncluded;
+    [SerializeField] TMP_InputField IFnameOfItem;
+    [SerializeField] TMP_InputField IFcoutOfItem;
 
-    [SerializeField] TMP_InputField IF;
-    
+
+    [Header("Cout of items list")]
+    public List<int> coutOfItemsList = new List<int>();
+    int lastAddedItemIndex;
+    int indexOfIncludedItem;
+    int howManyItems;
 
     public void ClickedAdd()
     {
+        howManyItems = int.Parse(IFcoutOfItem.text);
         if (userInputList.Contains(userInputText) == false)
         {
             userInputList.Add(userInputText);
+            coutOfItemsList.Add(howManyItems);
+        }
+        else if (userInputList.Contains(userInputText) == true)
+        {
+            indexOfIncludedItem = userInputList.IndexOf(userInputText);
+            coutOfItemsList[indexOfIncludedItem] += howManyItems;
         }
     }
 
@@ -24,15 +41,28 @@ public class Sklad : MonoBehaviour
     {
         if(userInputList.Contains(userInputText) == true)
         {
+            howManyItems = int.Parse(IFcoutOfItem.text);
             int whatToRemove = userInputList.IndexOf(userInputText);
-            userInputList.RemoveAt(whatToRemove);
+            if (coutOfItemsList[whatToRemove] == 1)
+            {
+                userInputList.RemoveAt(whatToRemove);
+                coutOfItemsList.RemoveAt(whatToRemove);
+            }
+            else if (coutOfItemsList[whatToRemove] > 1)
+            {
+                coutOfItemsList[whatToRemove] -= howManyItems;
+            }
+            
         }
     }
-    
-    public void EditedText()
+
+    public void EditedTextName()
     {
-        Debug.Log("Preslo to sem");
-        userInputText = IF.text.ToString();
-        Debug.Log(userInputText);        
+        userInputText = IFnameOfItem.text.ToString();
+    }
+    
+    public void EditedTextCount()
+    {
+        
     }
 }
