6e7752df04f2136863a381c6dc706675096b3dbe
ODLD25 <oliverdudovsky@gmail.com>
2025-11-24 15:45:24 +0100

    sgd

---
+++ Assets/Oliver/Spring/SpringsDemo.cs
@@ -0,0 +1,67 @@
+// fixed code for springs. Make sure the rigidbody itself, in the editor,  has linearDamping set to 0
+
+using UnityEngine;
+
+public class SpringsDemo : MonoBehaviour
+{
+    #region init
+    Rigidbody rb;
+    [SerializeField] float speed;
+    [SerializeField] float strength;
+    [SerializeField] float damping;
+    [SerializeField] Transform target;
+
+    [Header("Scaling")]
+    [SerializeField] Transform scaleVisual;
+    [SerializeField] AnimationCurve xyScaleByDistance;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+    #endregion init
+
+    void Update()
+    {
+        scaleVisual.LookAt(target, Vector3.right);
+
+        float dist = Vector3.Distance(transform.position, target.position);
+
+        scaleVisual.localScale = new Vector3(xyScaleByDistance.Evaluate(dist), xyScaleByDistance.Evaluate(dist), dist);
+        scaleVisual.position = (transform.position + target.position) / 2f;
+    }
+
+    void FixedUpdate()
+    {
+        // calculating velocity vector (only direction)
+        Vector3 translation = target.position - transform.position;
+
+        Vector3 directionToTarget = translation.normalized;
+        
+        // calculating the magnitude of the velocity vector
+        // a.k.a the spring force
+
+        float offset = Vector3.Distance(
+            transform.position, 
+            target.position);
+
+        float springForce = offset * strength;     
+
+        // calculate the damping force
+        Vector3 dampingForceVec3 = rb.linearVelocity * damping;
+
+
+        rb.linearVelocity += 
+            ((directionToTarget * 
+            springForce) - dampingForceVec3)
+            * Time.fixedDeltaTime;
+
+
+        Debug.Log(
+            $"spring: {springForce.ToString("F2")}" + 
+            $", vel: {rb.linearVelocity.magnitude.ToString("F2")}" +
+            $", damp: {dampingForceVec3.ToString("F2")}"
+        );
+    }
+}
