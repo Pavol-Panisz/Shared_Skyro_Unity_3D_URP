0d99b1d1402a140973e7c286100cbfc3f10623d8
ODLD25 <oliverdudovsky@gmail.com>
2025-12-05 15:15:54 +0100

    Procedural Generation
    10

--- Assets/Oliver/ProceduralGen/Scripts/MapGenerator.cs
+++ Assets/Oliver/ProceduralGen/Scripts/MapGenerator.cs
@@ -10,6 +10,8 @@ public class MapGenerator : MonoBehaviour
     public enum DrawMode{NoiseMap, ColorMap, Mesh}
     [SerializeField]private DrawMode drawMode;
 
+    [SerializeField]private Noise.NormalizeMode normalizeMode;
+
     public const int mapChunkSize = 241;
     [Range(0, 6)]public int editorPreviewLOD;
     [SerializeField]private float scale;
@@ -112,7 +114,7 @@ public class MapGenerator : MonoBehaviour
 
     private MapData GenerateMapData(UnityEngine.Vector2 centre)
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, scale, octaves, persistance, lacunarity, centre + offset);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, scale, octaves, persistance, lacunarity, centre + offset, normalizeMode);
 
         Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
         for (int y = 0; y < mapChunkSize; y++)
@@ -123,9 +125,12 @@ public class MapGenerator : MonoBehaviour
 
                 for (int i = 0; i < regions.Length; i++)
                 {
-                    if (currentHeight <= regions[i].height)
+                    if (currentHeight >= regions[i].height)
                     {
                         colorMap[y * mapChunkSize + x] = regions[i].color;
+                    }
+                    else
+                    {
                         break;
                     }
                 }
