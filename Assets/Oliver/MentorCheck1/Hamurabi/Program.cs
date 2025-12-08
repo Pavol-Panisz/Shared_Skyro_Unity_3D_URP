c23e6c2f9d8271fd8826f5f489145132ef811f71
ODLD25 <oliverdudovsky@gmail.com>
2025-12-08 18:21:34 +0100

    asfafs

--- Assets/Oliver/MentorCheck1/Hamurabi/Program.cs
+++ Assets/Oliver/MentorCheck1/Hamurabi/Program.cs
@@ -5,6 +5,7 @@
     public const int landPrice = 20;
     public const int startingBushels = 1000;
     public const int startingYear = 0;
+    public const int maxYear = 10;
 
     //GAME CURRENCY NAMES
     public const string moneyName = " bushels";
@@ -19,6 +20,10 @@
     public const string haveText = "You have ";
     public const string currentYearPrefix = "It is year ";
     public const string notEnoughMoneyText = "You don't have enough money!!!";
+    public const string canBuyText = "You can buy ";
+
+    //SUMMARY TEXT
+    public const string summaryText = "Summary";
 }
 
 public class MainProgram
@@ -36,17 +41,44 @@ public class MainProgram
 
     public static void Main()
     {
+        MainGame();
+    }
+
+    public static void MainGame()
+    {
         Report();
 
         TryToBuyLand();
+    
+        EndYear();
+    }
+
+    public static void EndYear()
+    {
+        if (currentYear > DefaultValues.maxYear)
+        {
+            EndGame();    
+        }
+        else
+        {
+            currentYear++;
+            MainGame();
+        }
+    }
+
+    public static void EndGame()
+    {
+        Report();
     }
 
     public static void Report()
     {
         //WRITE BASIC INFO
+        BonusMethods.PrintHeader(DefaultValues.summaryText);
+
         Console.WriteLine(DefaultValues.currentYearPrefix + currentYear);
         BonusMethods.PrintOwnedLand(ownedLand);
-        Console.WriteLine(DefaultValues.landCostText + landPrice + DefaultValues.landName);
+        Console.WriteLine(DefaultValues.landCostText + landPrice + DefaultValues.moneyName);
         BonusMethods.Printbushels(bushels);
 
         BonusMethods.Space();
@@ -54,7 +86,9 @@ public class MainProgram
 
     public static void TryToBuyLand()
     {
+        int amountThatPlayerCanBuy = bushels / landPrice;
         Console.WriteLine(DefaultValues.buyLandQuestion);
+        BonusMethods.PrintInParentheses(DefaultValues.canBuyText + " " + amountThatPlayerCanBuy.ToString());
 
         int amountOfLandToBuy = int.Parse(Console.ReadLine());
         
@@ -88,5 +122,24 @@ public class BonusMethods
     public static void PrintOwnedLand(int ownedLand)
     {
         Console.WriteLine(DefaultValues.ownText + ownedLand + DefaultValues.landName);
+    }
+
+    public static void PrintHeader(string text)
+    {
+        Space();
+        Console.WriteLine(text);
+        Space();
+    }
+
+    public static void PrintInParentheses(string text, bool writeOnNewLine = false)
+    {
+        if (writeOnNewLine)
+        {
+            Console.WriteLine(" (" + text + ")");
+        }
+        else
+        {
+            Console.Write(" (" + text + ")");
+        }
     }
 }
