cece6e4f64cef88f5e6a7656628783b01f52d048
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-12-19 09:46:24 +0100

    update projectile cs to damage players too

--- Shared_Skyro_Unity_3D_URP/Assets/PersonalAssets/Scripts/Projectile.cs
+++ Shared_Skyro_Unity_3D_URP/Assets/PersonalAssets/Scripts/Projectile.cs
@@ -3,19 +3,15 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public int damage = 10;
-    public string targetTag = "Enemy";
 
     void OnCollisionEnter(Collision collision)
     {
 
-        if (collision.gameObject.CompareTag(targetTag))
-        {
-            Health health = collision.gameObject.GetComponent<Health>();
+        Health health = collision.gameObject.GetComponent<Health>();
 
-            if (health != null)
-            {
-                health.TakeDamage(damage);
-            }
+        if (health != null)
+        {
+            health.TakeDamage(damage);
         }
 
         Destroy(gameObject);
