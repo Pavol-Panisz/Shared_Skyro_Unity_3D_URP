f51fda18fc3567ab5ac570b513ff2faf3e4627e5
Verygoodpizza <samuel-rajzinger@students.skyro.ai>
2026-01-30 11:30:16 +0100

    Scene changer a spawner loop animacia

--- Assets/MyScripts/SceneChanger.cs
+++ Assets/MyScripts/SceneChanger.cs
@@ -3,8 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    // Toto viem volaù z Unity Eventov
     public void LoadScene(string sceneName)
     {
+        // nasledovne 2 riadky su nove - naspat zapnu kurzor
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
         SceneManager.LoadScene(sceneName);
     }
 }
