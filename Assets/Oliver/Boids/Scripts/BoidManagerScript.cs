45b1cb520a30a004315dbc0a0efcd7bdff15f7c4
ODLD25 <oliverdudovsky@gmail.com>
2026-02-27 19:29:35 +0100

    WIP Cohesion

--- Assets/Oliver/Boids/Scripts/BoidManagerScript.cs
+++ Assets/Oliver/Boids/Scripts/BoidManagerScript.cs
@@ -3,10 +3,29 @@ using UnityEngine;
 
 public class BoidManagerScript: MonoBehaviour
 {
+    [Header("Boid Settings")]
     [SerializeField]private float boidSpeed;
     [SerializeField]private float changeTargetDist;
     [SerializeField]private float startSlowingRadius;
     [SerializeField]private float randomPosDist;
+    [SerializeField]private float seeRadius;
+    [SerializeField]private float rotationSpeed;
+
+    [Header("Separation Settings")]
+    [SerializeField]private bool separationEnabled;
+    [SerializeField]private float separationDistance;
+
+    [Header("Cohesion Settings")]
+    [SerializeField]private bool cohesionEnabled;
+
+    [Header("Chunks")]
+
+    [Header("Settings")]
+    [SerializeField]private DebugType debugType;
+    [SerializeField]private bool goToTarget;
+
+    [Header("References")]
+    [SerializeField]private Transform target;
     [SerializeField]private List<Rigidbody> boidsList = new List<Rigidbody>();
     private List<Vector3> targets = new List<Vector3>();
 
@@ -25,20 +44,92 @@ public class BoidManagerScript: MonoBehaviour
         int index = 0;
         float slowedSpeed;
         float dist;
+        Vector3 centerOfMass;
+
         foreach (Rigidbody rb in boidsList)
         {
+            //Calculate Values
             dist = Vector3.Distance(rb.transform.position, targets[index]);
             slowedSpeed = boidSpeed * dist / startSlowingRadius;
-            if (dist < changeTargetDist)
+            slowedSpeed = Mathf.Clamp(slowedSpeed, 0, boidSpeed);
+            centerOfMass = CalculateCenterOfMass(rb);
+
+            targets[index] = centerOfMass;
+
+            List<Vector3> dirs = new List<Vector3>();
+            if (separationEnabled)
             {
-                targets[index] = PickRandomPos();
+                //Get all directions around
+                foreach (Rigidbody boid in boidsList)
+                {
+                    if (boid == rb) continue;
+                    if (Vector3.Distance(rb.transform.position, boid.transform.position) < separationDistance)
+                    {
+                        dirs.Add(boid.transform.position - rb.transform.position);
+                    }
+                }
             }
 
-            rb.transform.LookAt(targets[index]);
-            rb.linearVelocity = rb.transform.forward * slowedSpeed;
+            Vector3 finalDir = Vector3.zero;
+            if (dirs.Count > 0)
+            {
+                foreach (Vector3 dir in dirs)
+                {
+                    finalDir += dir;
+                }
+                finalDir = -(finalDir / dirs.Count);    
+            }
+            else if (goToTarget)
+            {
+                finalDir = targets[index] - rb.transform.position;
+            }
+
+            targets[index] += finalDir;
+
+            switch (debugType)
+            {
+                case DebugType.RawDir:
+                    Debug.DrawRay(rb.transform.position, finalDir, Color.red, 0.5f);
+                break;
+                case DebugType.Target:
+                    Debug.DrawLine(rb.transform.position, targets[index], Color.blue, 0.5f);
+                break;
+                case DebugType.RawDirAndTarget:
+                    Debug.DrawRay(rb.transform.position, finalDir, Color.red, 0.5f);
+                    Debug.DrawLine(rb.transform.position, targets[index], Color.blue, 0.5f);
+                break;
+                case DebugType.Everything:
+                    Debug.DrawRay(rb.transform.position, finalDir, Color.red, 0.5f);
+                    Debug.DrawLine(rb.transform.position, targets[index], Color.blue, 0.5f);
+                break;
+            }
+
+            //Change Rot
+            rb.transform.forward = Vector3.RotateTowards(rb.transform.position, targets[index], rotationSpeed * Mathf.Deg2Rad, Mathf.Infinity);
+
+            //Set Velocity
+            rb.linearVelocity = rb.transform.forward * boidSpeed;
 
             index++;
-        }        
+        }
+    }
+
+    private Vector3 CalculateCenterOfMass(Rigidbody boidRb)
+    {
+        Vector3 centerOfMass = Vector3.zero;
+        int index = 0;
+        foreach (Rigidbody rb in boidsList)
+        {
+            if (Vector3.Distance(rb.transform.position, boidRb.transform.position) < seeRadius)
+            {
+                centerOfMass += rb.transform.position;
+                index++;
+            }
+        }
+
+        centerOfMass = centerOfMass / index;
+
+        return centerOfMass;
     }
 
     private Vector3 PickRandomPos()
@@ -50,7 +141,49 @@ public class BoidManagerScript: MonoBehaviour
     {
         for (int i = 0; i < boidsList.Count; i++)
         {
-            targets.Add(PickRandomPos());
+            targets.Add(target.position);
         }
     }
+
+    void OnDrawGizmos()
+    {
+        if (debugType == DebugType.Everything)
+        {
+            foreach (Rigidbody rb in boidsList)
+            {
+                Gizmos.color = Color.white;
+                Gizmos.DrawWireSphere(rb.transform.position, seeRadius);
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(CalculateCenterOfMass(rb), 0.5f);
+            }
+        }
+        if (debugType == DebugType.SeeRadius)
+        {
+            Gizmos.color = Color.white;
+            foreach (Rigidbody rb in boidsList)
+            {
+                Gizmos.DrawWireSphere(rb.transform.position, seeRadius);
+            }
+        }
+        else if (debugType == DebugType.CenterOfMass)
+        {
+            Gizmos.color = Color.yellow;
+            foreach (Rigidbody rb in boidsList)
+            {
+                Gizmos.DrawWireSphere(CalculateCenterOfMass(rb), 0.5f);
+            }
+        }
+
+    }
+}
+
+enum DebugType
+{
+    None,
+    RawDir,
+    Target,
+    RawDirAndTarget,
+    CenterOfMass,
+    SeeRadius,
+    Everything
 }
