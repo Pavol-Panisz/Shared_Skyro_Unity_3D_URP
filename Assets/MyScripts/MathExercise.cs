712bb6e468019c31a27b94a6256464f5dc2cdefd
Verygoodpizza <samuel-rajzinger@students.skyro.ai>
2026-03-13 10:33:10 +0100

    bounces count script

---
+++ Assets/MyScripts/MathExercise.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class MathExercise : MonoBehaviour
+{
+    public int bouncesCount = 0 ;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        bouncesCount++;
+
+        if (bouncesCount % 3 == 0 )
+
+        {
+            Debug.Log("Fizz");
+        }    
+        else
+        {
+            Debug.Log(bouncesCount);
+        }
+
+
+
+    }
+
+
+
+
+    void Update()
+    {
+        
+
+
+    }
+}
