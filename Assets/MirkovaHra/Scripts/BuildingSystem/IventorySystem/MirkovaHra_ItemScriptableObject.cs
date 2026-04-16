19b4b774f62764be90656a7d353f9c829d3df0f0
eugeniy3339 <eugeniydurbalo@gmail.com>
2026-04-16 12:11:40 +0200

    Cleared the project

--- Assets/MirkovaHra/Scripts/BuildingSystem/IventorySystem/MirkovaHra_ItemScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "newItem", menuName = "Mirkova Hra/Inventory System/Create new Item")]
-public class MirkovaHra_ItemScriptableObject : ScriptableObject
-{
-    public string name;
-    public string description;
-    public Sprite itemSprite;
-}
--- Assets/MirkovaHra/Scripts/BuildingSystem/MirkovaHra_BuildingsList.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "newBuildingsList", menuName = "Mirkova Hra/Building System/Create new Buildings List")]
-public class MirkovaHra_BuildingsList : ScriptableObject
-{
-    public List<MirkovaHra_BuildingScriptableObject> buildings;
-}
